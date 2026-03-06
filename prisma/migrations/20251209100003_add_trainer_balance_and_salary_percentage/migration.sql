f36f8f3ae2652feda3c92ad34ba9d72238573e2a
adrastion <adrastion@gmail.com>
2026-03-06 03:27:29 +0300

    Миграция

---
+++ prisma/migrations/20251209100003_add_trainer_balance_and_salary_percentage/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "trainers" ADD COLUMN IF NOT EXISTS "balance" DECIMAL(65,30) NOT NULL DEFAULT 0;
+ALTER TABLE "trainers" ADD COLUMN IF NOT EXISTS "salaryPercentage" DECIMAL(65,30);
