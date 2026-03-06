9641f29aad3cf3330346d9c326f4b7f3d6dfb0c4
adrastion <adrastion@gmail.com>
2026-03-06 03:17:12 +0300

    миграция

--- prisma/migrations/$(date +%Y%m%d%H%M%S)_add_onboarding_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "tenant_settings" ADD COLUMN IF NOT EXISTS "hasCompletedOnboarding" BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE "tenant_settings" ADD COLUMN IF NOT EXISTS "onboardingDeclined" BOOLEAN NOT NULL DEFAULT false;
-
--- prisma/migrations/$(date +%Y%m%d%H%M%S)_add_timezone_to_trainer_notification_settings/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "trainer_notification_settings" ADD COLUMN IF NOT EXISTS "timezone" TEXT DEFAULT 'UTC';
-
